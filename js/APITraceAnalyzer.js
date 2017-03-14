0c37c350815e019d891f7961f12d32b780449f63
Chih-Yung Liang <r05944012@csie.ntu.edu.tw>
2017-03-15 05:30:29 +0800

    Add kernel time analysis.
    There are lots of code duplication.

--- js/APITraceAnalyzer.js
+++ js/APITraceAnalyzer.js
@@ -57,6 +57,7 @@ function APITraceAnalyzer(updateFrequency, reportDiv) {
     this.report = function() {
         this.reportDiv.empty();
         this.analyzeResult.apiTime.report(reportDiv);
+        this.analyzeResult.kernelTime.report(reportDiv);
     };
 
     function APIOutput(data, analyzer) {
@@ -217,10 +218,152 @@ function APITraceAnalyzer(updateFrequency, reportDiv) {
     };
 
     function KernelTime(data, analyzer) {
-        var lines = data.split("\n");
-        var count = lines[1];
+        let lines = data.split("\n");
+        let count = lines[1];
 
         this.analyzer = analyzer;
+        this.kernelDurations = {};
+        this.div = null;
+        this.plotDiv = null;
+
+        lines = lines.slice(2, -1);
+        for (let launch in lines) {
+            let fields = lines[launch].split(/ +/);
+            let duration = fields[3] - fields[2];
+
+            if (this.kernelDurations[fields[0]] == undefined) {
+                this.kernelDurations[fields[0]] = [duration];
+            } else {
+                this.kernelDurations[fields[0]].push(duration);
+            }
+        }
+
+        let _this = this;
+        this.statisticResult = $.map(this.kernelDurations, function(value, key) {
+            let durations = jStat(value);
+
+            durations._kernelName = key;
+            durations.kernelName = function() {
+                return this._kernelName;
+            };
+
+            durations.ratio = function() {
+                return (this.sum() / _this.totalTime * 100).toFixed(4) + "%";
+            };
+
+            return durations;
+        });
+        this.totalTime = this.statisticResult.reduce(function(total, currentValue) {
+            return total + currentValue.sum();
+        }, 0);
+
+        this.sort = function() {
+            if (this.analyzer.sortKernelTime !== undefined) {
+                let sort = this.analyzer.sortKernelTime;
+                let column = sort.slice(1);
+                let order = Number(sort[0] + '1');
+
+                this.statisticResult.sort(function(l, r) {
+                    return (r[column]() - l[column]()) * order;
+                });
+            }
+        };
+        this.sort();
+
+        this.report = function(div) {
+            if (this.div == null) {
+                let title = $("<h3>Kernel Times Analysis</h3>").appendTo(div);
+
+                let _this = this;
+                $('<a class="pull-right btn btn-sm btn-default" href="#">Toggle hide</a>').appendTo(title).click(function() {
+                    _this.div.slideToggle();
+                });
+
+                this.div = $('<div>').appendTo(div);
+            } else {
+                this.div.empty();
+            }
+
+            this.plotDiv = $('<div>').appendTo(this.div);
+
+            this.drawTable();
+        };
+
+        this.drawTable = function() {
+            let table = $('<table class="table table-striped table-hover">').appendTo(this.div);
+            let columns = {
+                kernelName: "Kernel",
+                cols: "Count",
+                min: "Min",
+                max: "Max",
+                median: "Median",
+                stdev: "Standard Deviation",
+                sum: "Cumulative",
+                ratio: "%"
+            };
+
+            let theadr = $('<tr>').appendTo($('<thead>').appendTo(table));
+            let _this = this;
+            $.each(columns, function(key, value) {
+                let sort = _this.analyzer.sortKernelTime;
+                if (sort !== undefined && sort.slice(1) == key) {
+                    value = sort[0] + value;
+                    key = sort[0] + key;
+                }
+
+                $('<a>').appendTo($('<th>').appendTo(theadr))
+                    .text(value)
+                    .attr('href', '#')
+                    .data('column', key)
+                    .click(function() {
+                        if (key[0] == '-') {
+                            _this.analyzer.sortKernelTime = '+' + $(this).data('column').slice(1);
+                        } else if (key[0] == '+') {
+                            _this.analyzer.sortKernelTime = '-' + $(this).data('column').slice(1);
+                        } else {
+                            _this.analyzer.sortKernelTime = '-' + $(this).data('column');
+                        }
+
+                        _this.sort();
+                        _this.report();
+                    });
+            });
+            $('<th>').appendTo(theadr).text("Plot");
+
+            let tbody = $('<tbody>').appendTo(table);
+            $.each(this.statisticResult, function(index, result) {
+                let tr = $('<tr>').appendTo(tbody);
+                $.each(columns, function(key, value) {
+                    let number = result[key]();
+                    if (Number(number) === number) {
+                        number = (Math.round(number * 10000) / 10000).toLocaleString();
+                    }
+
+                    $('<td>').appendTo(tr).html(number);
+                });
+
+                $('<a>').appendTo($('<td>').appendTo(tr))
+                    .text('Draw')
+                    .attr('href', '#')
+                    .data('kernel', result.kernelName())
+                    .click(function() {
+                        _this.drawPlot($(this).data('kernel'));
+                    });
+            });
+        };
+
+        this.drawPlot = function(kernelName) {
+            let data = this.kernelDurations[kernelName];
+            this.plotDiv.empty();
+
+            Plotly.newPlot(this.plotDiv[0], [{
+                x: data,
+                type: 'histogram',
+                marker: {
+                    color: 'rgba(100, 250, 100, 0.7)'
+                }
+            }]);
+        };
     }
 }
 
